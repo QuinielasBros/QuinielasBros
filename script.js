b9cc53bd14f64d263b4e9ba624fa42f6299e2565
QuinielasBros <kassandrazamorar@gmail.com>
2025-02-27 00:49:22 -0600

    cambiando el diseño y agregando imagenes

--- script.js
+++ script.js
@@ -12,15 +12,20 @@ function mostrarDatos(lista, idDiv) {
     let container = document.getElementById(idDiv);
     lista.forEach(item => {
         let card = document.createElement("div");
-        card.classList.add("card");
+        let claseBanco = obtenerClaseBanco(item.banco);
+
+        card.classList.add("card", claseBanco);
+
         card.innerHTML = `
-            <div>
+            <img src="${item.imagen}" alt="${item.banco}">
+            <div class="card-content">
                 <h4>${item.banco}</h4>
                 <p><strong>${item.numero}</strong></p>
                 <p>${item.titular}</p>
             </div>
             <button onclick="copiarTexto('${item.numero}')">Copiar</button>
         `;
+
         container.appendChild(card);
     });
 }
@@ -31,4 +36,12 @@ function copiarTexto(texto) {
     }).catch(err => {
         console.error("Error al copiar: ", err);
     });
+}
+
+function obtenerClaseBanco(banco) {
+    if (banco.includes("BBVA")) return "bbva";
+    if (banco.includes("BANCOOPEL")) return "bancoppel";
+    if (banco.includes("HSBC")) return "hsbc";
+    if (banco.includes("OXXO")) return "oxxo";
+    return "";
 }
